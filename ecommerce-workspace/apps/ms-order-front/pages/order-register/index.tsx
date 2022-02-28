fb1142f583f0deeaa596eb27b81ffa4a7a0a5664
janderson.rosa <janderson.rosa@cwi.com.br>
2022-02-28 17:40:05 -0300

    initisl

---
+++ ecommerce-workspace/apps/ms-order-front/pages/order-register/index.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+/* eslint-disable-next-line */
+export interface OrderRegisterProps {}
+
+const StyledOrderRegister = styled.div`
+  color: pink;
+`;
+
+export function OrderRegister(props: OrderRegisterProps) {
+  return (
+    <StyledOrderRegister>
+      <h1>Welcome to OrderRegister!</h1>
+    </StyledOrderRegister>
+  );
+}
+
+export default OrderRegister;
